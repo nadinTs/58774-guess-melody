e1376aadf0e5e8e5699bcf4c6f461d9f9a5a4603
Nadya <nadin.ts@gmail.com>
2017-05-26 10:24:58 +0300

    сделана функция переключения экранов

---
+++ js/main.js
@@ -0,0 +1,32 @@
+(function () {
+  'use strict';
+
+  const app = document.querySelector('div.app');
+  const main = app.querySelector('section.main');
+  const template = document.querySelector('template');
+  const templateContainer = 'content' in template ? template.content : template;
+  const screen = templateContainer.querySelectorAll('section.main');
+  const screenArr = [];
+
+  [].forEach.call(screen, function (div) {
+    screenArr.splice(0, 0, div);
+  });
+  const reversScreen = screenArr.splice(4, 1);
+  screenArr.splice(1, 0, reversScreen[0]);
+
+  let element = main.appendChild(screenArr[0]);
+  let i = 0;
+  const changeScreen = function (event) {
+    if (event.altKey && event.keyCode === 39) {
+      if (i < screenArr.length - 1) {
+        i++;
+      } else {
+        i = 0;
+      }
+      main.replaceChild(screenArr[i], element);
+      element = screenArr[i];
+    }
+  };
+
+  document.body.onkeydown = changeScreen;
+})();
