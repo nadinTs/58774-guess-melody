99c6425ac1528213f2ac9be678984c50330c3999
Nadya <nadin.ts@gmail.com>
2017-06-16 14:27:25 +0300

    Доработала функцию changeScreen.

--- js/changeScreen.js
+++ js/changeScreen.js
@@ -1,9 +1,13 @@
-export default (element) => {
-  const app = document.querySelector(`div.app`);
-  const main = app.querySelector(`section.main`);
+const templ = (element) => {
   const section = document.createElement(`section`);
   section.className = `main`;
   section.innerHTML = element;
+  return section;
+};
+
+export default (element) => {
+  const app = document.querySelector(`div.app`);
+  const main = app.querySelector(`section.main`);
   app.removeChild(main);
-  app.prepend(section);
+  app.prepend(templ(element));
 };
