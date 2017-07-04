52d67b9f09ff640857eb9c3be527f333d4f89e97
Nadya <nadin.ts@gmail.com>
2017-07-04 12:37:30 +0300

    убрала location.reload

--- js/game/game.js
+++ js/game/game.js
@@ -89,7 +89,6 @@ export default class Game {
   setResult() {
     const view = new ResultView(this.correctAnswers, this.timer.seconds, this.statsData);
     view.onAnswer = () => {
-      location.reload();
       app.showWelcome();
     };
     changeScreen(view.element);
